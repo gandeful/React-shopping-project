6f5a864e81e69e329d50a8c89efd37d6c2cdf324
ganful <863529565@qq.com>
2018-06-16 00:51:56 +0800

    学习ES6

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.less';
-import { NavBar, Icon } from 'antd-mobile';
+import { NavBar, Flex,WhiteSpace,WingBlank } from 'antd-mobile';
 
-class FJH extends Component{
-  render(){
-    return(
-      <div className={styles.title}>
-      <div>back</div>
-      <div>title</div>
-      <div>right</div>
-      </div>
-    )
-  }
-}
+const renderRow = (key,left,center,right) =>(
+  <div key={key}>
+<WhiteSpace size="sm" />
+<WingBlank size="lg">
+  <Flex >
+  <Flex.Item>{left}</Flex.Item>
+  <Flex.Item  className='text-center'>{center}</Flex.Item>
+  <Flex.Item  className='text-right'>{right}</Flex.Item>
+</Flex>
+</WingBlank>
+  </div>
+ 
+) 
 class App extends Component {
   render() {
-    
+    const arr =[
+      ['交易时间','交易笔数','交易金额'],
+      ['昨日','100','100000'],
+      ['今日','100','10000']
+    ]
+
     return (
       
-      <div className={styles.App}>
-      <FJH />
+      <div>
       <NavBar
-      mode="dark"
-      leftContent="Back"
-      rightContent={[
-        <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
-        <Icon key="1" type="ellipsis" />,
-      ]}
-    >NavBar</NavBar>
-        <header className={styles.Appheader}>
-          <img src={logo} className={styles.Applogo} alt="logo" />
-          <h1 className={styles.Apptitle}>Welcome to React,</h1>
-        </header>
-        <p className={styles.Appintro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      mode="light"
+    >酒店名称</NavBar>
+    <WhiteSpace size="lg" />
+    <WingBlank size="lg">
+    交易汇总
+    </WingBlank>
+      {
+        arr.map((v,i)=>(renderRow(i,...v)))
+      }
       </div>
         
       
