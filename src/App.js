973130f660d26c1ba96ff3fd7873685711da046b
ganful <863529565@qq.com>
2018-06-16 21:57:23 +0800

    Grid完成首页

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styles from './App.less';
-import { NavBar, Flex,WhiteSpace,WingBlank } from 'antd-mobile';
+import { NavBar, Flex,WhiteSpace,WingBlank ,Grid} from 'antd-mobile';
+
+
 
 const renderRow = (key,left,center,right) =>(
   <div key={key}>
@@ -13,8 +15,29 @@ const renderRow = (key,left,center,right) =>(
 </Flex>
 </WingBlank>
   </div>
- 
-) 
+)
+const icon = 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png';
+const data = [
+    {
+        icon,
+        text:'销售'
+    },{
+        icon,
+        text:'采购'
+    },{
+        icon,
+        text:'入库'
+    },{
+        icon,
+        text:'商品管理'
+    },{
+        icon,
+        text:'库存管理'
+    },{
+        icon,
+        text:'报表'
+    }
+];
 class App extends Component {
   render() {
     const arr =[
@@ -25,7 +48,7 @@ class App extends Component {
 
     return (
       
-      <div>
+      <div className={styles.app}>
       <NavBar
       mode="light"
     >酒店名称</NavBar>
@@ -36,6 +59,7 @@ class App extends Component {
       {
         arr.map((v,i)=>(renderRow(i,...v)))
       }
+          <Grid className={styles.grid} data={data} activeStyle={false} columnNum={3} />
       </div>
         
       
